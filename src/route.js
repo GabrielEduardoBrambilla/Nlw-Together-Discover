2f28e6f899589bb3de4693be85b5c30e0dcbd820
GabrielEduardoBrambilla <55161971+GabrielEduardoBrambilla@users.noreply.github.com>
2022-05-14 19:41:15 -0300

    Node

---
+++ src/route.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const route = express.Router()
+
+route.get('/', (req, res) => res.render('index', { page: 'enter-room' }))
+route.get('/create-pass', (req, res) =>
+  res.render('index', { page: 'create-pass' })
+)
+
+module.exports = route
