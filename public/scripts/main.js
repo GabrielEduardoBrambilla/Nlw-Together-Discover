708c44d3add9bea0044cdb92e38c7c62482c09b3
GabrielEduardoBrambilla <55161971+GabrielEduardoBrambilla@users.noreply.github.com>
2022-05-29 19:58:24 -0300

    Update main.js

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -9,19 +9,20 @@ const modalButton = document.querySelector('.modal button')
 const checkButtons = document.querySelectorAll('.actions a.check')
 
 checkButtons.forEach(button => {
-  button.addEventListener('click', event => {
-    modalTitle.innerHTML = 'Marcar como lida'
-
-    modal.open()
-  })
+  button.addEventListener('click', handleClick)
 })
 
 const deleteButton = document.querySelectorAll('.actions a.delete')
 
 deleteButton.forEach(button => {
-  button.addEventListener('click', event => {
-    modalTitle.innerHTML = 'Excluir essa pergunta'
-
-    modal.open()
-  })
+  button.addEventListener('click', event => handleClick(event, false))
 })
+
+function handleClick(event, check = true) {
+  const text = check ? 'Marcar como lida' : 'Excluir'
+
+  modalTitle.innerHTML = `${text} esta pergunta`
+  modalDescription.innerHTML = `Tem certeza que deeja ${text.toLowerCase()} esta pergunta?`
+
+  modal.open()
+}
