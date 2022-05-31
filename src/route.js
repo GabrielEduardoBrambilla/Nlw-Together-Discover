832473f5c8719861a49e632fdca95906fe87e4b6
GabrielEduardoBrambilla <55161971+GabrielEduardoBrambilla@users.noreply.github.com>
2022-05-31 11:50:51 -0300

    Optimizing pages

--- src/route.js
+++ src/route.js
@@ -3,9 +3,13 @@ const questionController = require('./controllers/questionController')
 
 const route = express.Router()
 
-route.get('/', (req, res) => res.render('index'))
+route.get('/', (req, res) => res.render('index', { page: 'enter-room' }))
+
+route.get('/create-pass', (req, res) =>
+  res.render('index', { page: 'create-pass' })
+)
+
 route.get('/room', (req, res) => res.render('room'))
-route.get('/create-pass', (req, res) => res.render('create-pass'))
 
 route.post('/room/:room/:question/:action', questionController.index)
 
