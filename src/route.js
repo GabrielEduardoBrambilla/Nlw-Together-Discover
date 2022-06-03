f74c7c3ef52c4ef975f83041641fb9214a3159ca
GabrielEduardoBrambilla <55161971+GabrielEduardoBrambilla@users.noreply.github.com>
2022-06-02 23:01:42 -0300

    References fixes

--- src/route.js
+++ src/route.js
@@ -10,14 +10,10 @@ route.get('/create-pass', (req, res) =>
   res.render('index', { page: 'create-pass' })
 )
 
-route.get('/room', (req, res) => res.render('room'))
+route.get('/room/:room', (req, res) => res.render('room'))
 
 route.post('/room/:room/:question/:action', questionController.index)
 route.post('/room/create-room', RoomController)
 
-// route.get('/', (req, res) => res.render('room', { page: 'enter-room' }))
-// route.get('/create-pass', (req, res) =>
-//   res.render('index', { page: 'create-pass' })
-// )
 
 module.exports = route
